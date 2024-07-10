56eb169d47300b66a8f3f4aaf2984e5fef337d73
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-10 23:02:10 +0100

    Add game logic to compare user ochoicewith computer

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     let computerChoice;
 
+    // convert number to game option
     switch (randomNumber) {
         case 1:
             computerChoice = "Rock";
@@ -25,13 +26,37 @@ function getComputerChoice() {
 
 function getHumanChoice() {
 
+    // ask user for input and set lowercase format
     let humanChoice = prompt("Rock, Paper or Scissors?");
     humanChoice = humanChoice.toLowerCase();
 
+    // if user input is invalid re-call the function
     if ((humanChoice != "rock") && (humanChoice != "paper") && (humanChoice != "scissors")) {
         return getHumanChoice();
     } else {
         return humanChoice;
+    }
+
+}
+
+function playRound(computerChoice, humanChoice) {
+    
+    if (computerChoice == humanChoice) {
+        console.log(`It's a draw ${computerChoice} matches ${humanChoice}`);
+    } else if (
+        (computerChoice == "rock" && humanChoice == "scissors") ||
+        (computerChoice == "paper" && humanChoice == "rock") ||
+        (computerChoice == "scissors" && humanChoice == "paper")
+    ) {
+        console.log(`The computer wins, ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log(`The computer wins, ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
     }
 
 }
