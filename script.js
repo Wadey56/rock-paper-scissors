c010a996ad96c959e0f7f1dcf9f025b79071dca0
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-07-10 22:02:49 +0100

    Convert random number to Rock, Paper or Scissors

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function getComputerChoice() {
 
-// get a random number between 1 and 3
-let randomNumber = Math.floor(Math.random() * 3 + 1)
+    // get a random number between 1 and 3
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    let computerChoice;
+
+    switch (randomNumber) {
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors"
+            break;
+    }
+
+    return computerChoice;
+
 }
